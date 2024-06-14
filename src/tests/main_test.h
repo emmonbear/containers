080a102fd8c2fda77804411ae0c39bd1a2b11c48
emmonbea <moskaleviluak@icloud.com>
2024-06-14 23:37:22 +0500

    test: add main test runner and header file

---
+++ src/tests/main_test.h
@@ -0,0 +1,22 @@
+/**
+ * @file main_test.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-06-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_TESTS_MAIN_TEST_H_
+#define SRC_TESTS_MAIN_TEST_H_
+
+#include <gtest/gtest.h>
+
+#include "../s21_containers.h"
+
+// Switch on if you want to print the matrix
+// #define ENABLE_DEBUG_PRINTS
+
+#endif  // SRC_TESTS_MAIN_TEST_H_

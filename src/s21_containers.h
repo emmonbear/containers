9bae451ded2739f8dd923e2399e07da4bfb6b757
emmonbea <moskaleviluak@icloud.com>
2024-06-14 22:09:46 +0500

    feat(containers): add header file to aggregate container classes

---
+++ src/s21_containers.h
@@ -0,0 +1,22 @@
+/**
+ * @file s21_containers.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief A header file that combines the various container classes
+ * @version 1.0
+ * @date 2024-06-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef SRC_S21_CONTAINERS_H_
+#define SRC_S21_CONTAINERS_H_
+
+#include "include/list.h"
+#include "include/map.h"
+#include "include/queue.h"
+#include "include/set.h"
+#include "include/stack.h"
+#include "include/vector.h"
+
+#endif  // SRC_S21_CONTAINERS_H_

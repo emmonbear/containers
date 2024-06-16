a9e6b2d5cfd2e0e73f99ee58156de05715d907f3
emmonbea <moskaleviluak@icloud.com>
2024-06-16 23:24:21 +0500

    feat(list): add initial declaration of list class
    - Define list class with basic functionalities and type definitions
    
    - Implement constructors, copy/move operations, and destructor

--- src/include/list.h
+++ src/include/list.h
@@ -12,4 +12,40 @@
 #ifndef SRC_INCLUDE_LIST_H_
 #define SRC_INCLUDE_LIST_H_
 
+namespace containers {
+/**
+ * @brief Class that implements a list.
+ *
+ * @details
+ *
+ * List is a sequence container that stores a set of elements with arbitrary
+ * size, in the form of nodes connected in sequence by pointers. Each node
+ * stores a value corresponding to an element in the list, and a pointer to the
+ * next element. This container design allows you to avoid a rigidly fixed size,
+ * such as in a static array, and makes adding a new element to the container
+ * more user-friendly.
+ *
+ */
+template <typename T>
+class list {
+ public:
+  // types
+  using value_type = T;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using size_type = std::size_t;
+  // using iterator = /* implementation-defined */;
+  // using const_iterator = /* implementation-defined */;
+
+  // construct/copy/destroy
+  list();
+  explicit list(size_type n);
+  list(std::initializer_list<value_type> const &items);
+  list(const list &l);
+  list(list &&l);
+  ~list() noexcept;
+  list operator=(list &&l);
+};
+}  // namespace containers
+
 #endif  // SRC_INCLUDE_LIST_H_

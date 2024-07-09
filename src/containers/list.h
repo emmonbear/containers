316e50a8a97a2698467cb61289a6c37f116319ea
emmonbea <moskaleviluak@icloud.com>
2024-07-09 15:24:22 +0500

    feat(list): add new methods

--- src/containers/list.h
+++ src/containers/list.h
@@ -31,43 +31,60 @@ namespace s21 {
 template <typename T>
 class list {
  public:
-  // types
-  using value_type = T;
-  using reference = value_type &;
-  using const_reference = const value_type &;
-  using size_type = std::size_t;
-  // using iterator = /* implementation-defined */;
-  // using const_iterator = /* implementation-defined */;
+  // TYPES AND TYPE ALIASES
 
-  // construct/copy/destroy
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = std::size_t;
+
+  // CONSTRUCTORS AND ASSIGNMENT OPERATORS
+
   list() noexcept;
-  explicit list(size_type n) noexcept;
-  list(std::initializer_list<value_type> const &items);
   list(const list &l);
+  list &operator=(const list &l);
   list(list &&l);
-  ~list() noexcept;
   list operator=(list &&l);
+  // explicit list(size_type n) noexcept;
+  // list(std::initializer_list<value_type> const &items);
+
+  // DESTRUCTOR
+
+  ~list() noexcept;
+
+  // METHODS
+
+  bool operator==(const list &l) const;
 
   void push_back(const_reference value) noexcept;
+  void pop_back() noexcept;
 
   bool empty() noexcept;
+  size_type size() const;
+
+  void clear() noexcept;
 
  private:
+  // LIST NODE
+
   struct Node {
     value_type value_;
     Node *prev_{nullptr};
     Node *next_{nullptr};
 
-    explicit Node(const value_type &value) : value_{value} {}
+    explicit Node(const_reference value)
+        : value_{value}, prev_{nullptr}, next_{nullptr} {}
   };
 
   Node *head_{nullptr};
   Node *tail_{nullptr};
   size_type size_{0};
 
-  void clear() noexcept;
-  void pop_back() noexcept;
+  // SUPPORTING METHODS
+  void copy_from(const list &l);
 };
+
+///////////////////////////////////////////////////////////////////////////////
 
 /**
  * @brief Default constructor for the list class.
@@ -82,18 +99,33 @@ class list {
 template <typename value_type>
 list<value_type>::list() noexcept = default;
 
-// template <typename value_type>
-// list<value_type>::list(size_type n) noexcept
-//     : head_{nullptr}, tail_{nullptr}, size_{0} {
-//   if (n > max_size()) {
-//     std::cerr << "Requested size exceeds maximum allowable size." <<
-//     std::endl; exit 1;
-//   }
-//   for (size_type i = 0; i < n; i++) {
-//     push_back(value_type());
-//   }
-// }
+/**
+ * @brief Copy constructor for creating a new list as a copy of another list.
+ *
+ * @details
+ *
+ * This constructor initializes a new list as a copy of the provided list `l`.
+ * It initializes the head, tail, and size of the new list and then copies all
+ * elements from `l` to the new list using the `copy_from` method.
+ * @param l The list to be copied.
+ * @tparam value_type The type of elements stored in the list.
+ */
+template <typename value_type>
+list<value_type>::list(const list &l)
+    : head_{nullptr}, tail_{nullptr}, size_{0} {
+  copy_from(l);
+}
 
+/**
+ * @brief Destructor for the list class.
+ *
+ * @details
+ *
+ * The destructor calls the clear() method to free all elements of the list,
+ * preventing memory leaks
+ *
+ * @tparam value_type The type of elements stored in the list.
+ */
 template <typename value_type>
 list<value_type>::~list() noexcept {
   clear();
@@ -113,12 +145,7 @@ list<value_type>::~list() noexcept {
  */
 template <typename value_type>
 void list<value_type>::push_back(const_reference value) noexcept {
-  Node *new_node = new (std::nothrow) Node(value);
-
-  if (!new_node) {
-    std::cerr << "Error: Failed to allocate memory for new node." << std::endl;
-    return;
-  }
+  Node *new_node = new Node{value};
 
   if (!head_) {
     head_ = new_node;
@@ -134,8 +161,7 @@ void list<value_type>::push_back(const_reference value) noexcept {
 
 /**
  * @brief Checks whether the container is empty.
- *
- * @return * template <typename value_type>
+ * @tparam value_type The type of elements stored in the list.
  */
 template <typename value_type>
 bool list<value_type>::empty() noexcept {
@@ -144,8 +170,7 @@ bool list<value_type>::empty() noexcept {
 
 /**
  * @brief Clear the contents of the list.
- *
- * @return * template <typename value_type>
+ * @tparam value_type The type of elements stored in the list.
  */
 template <typename value_type>
 void list<value_type>::clear() noexcept {
@@ -156,8 +181,7 @@ void list<value_type>::clear() noexcept {
 
 /**
  * @brief Removes the last element of the list.
- *
- * @return * template <typename value_type>
+ * @tparam value_type The type of elements stored in the list.
  */
 template <typename value_type>
 void list<value_type>::pop_back() noexcept {
@@ -177,6 +201,69 @@ void list<value_type>::pop_back() noexcept {
 
     size_--;
   }
+}
+
+/**
+ * @brief Returns the number of elements in the list.
+ * @return size_type The number of elements in the list.
+ * @tparam value_type The type of elements stored in the list.
+ */
+template <typename value_type>
+typename list<value_type>::size_type list<value_type>::size() const {
+  return size_;
+}
+
+/**
+ * @brief Copies elements from another list to the current list.
+ *
+ * @details
+ *
+ * This method copies all elements from the provided list `l` to the current
+ * list. It iterates through the elements of `l` and appends each element to the
+ * current list.
+ *
+ * @param l The list from which elements will be copied.
+ * @tparam value_type The type of elements stored in the list.
+ */
+template <typename value_type>
+void list<value_type>::copy_from(const list &l) {
+  Node *current = l.head_;
+
+  while (current) {
+    push_back(current->value_);
+    current = current->next_;
+  }
+}
+
+/**
+ * @brief Equality operator to compare two lists.
+ *
+ * @param l The list to compare with the current list.
+ * @tparam value_type The type of elements stored in the list.
+ * @retval true if both lists have the same size and all corresponding elements
+ * are equal.
+ * @retval false if the sizes of the lists are different or if at least one
+ * element differs.
+ */
+template <typename value_type>
+bool list<value_type>::operator==(const list &l) const {
+  if (size_ != l.size_) {
+    return false;
+  }
+
+  Node *current_1 = head_;
+  Node *current_2 = l.head_;
+
+  while (current_1 && current_2) {
+    if (current_1->value_ != current_2->value_) {
+      return false;
+    }
+
+    current_1 = current_1->next_;
+    current_2 = current_2->next_;
+  }
+
+  return true;
 }
 
 }  // namespace s21
